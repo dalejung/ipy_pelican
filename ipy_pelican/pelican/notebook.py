b397c0ce104bc5e5051df905b8bbc2d049c296fa
Dale Jung <dale@dalejung.com>
2013-06-10 19:53:26 -0400

    moving around, occurs ot me that the function split might not make as much sense anymore

--- ipy_pelican/pelican/notebook.py
+++ ipy_pelican/pelican/notebook.py
@@ -136,25 +136,11 @@ def notebook(preprocessor, tag, markup):
         refresh = False
 
     settings = preprocessor.configs.config['settings']
-    body_lines = _notebook(settings, src, refresh=refresh, start=start, end=end, name=name)
+    notebook_output = generate_notebook_output(settings, src, refresh=refresh, start=start, end=end, name=name)
+    body_lines, header_lines, resources, asset_path = notebook_output
 
-    body = preprocessor.configs.htmlStash.store('\n'.join(body_lines),
-                                                safe=True)
-    return body
-
-def _notebook(settings, src, refresh=False, start=None, end=None, name=None):
-    nb_dir =  settings.get('NOTEBOOK_DIR', 'notebooks')
-    nb_path = os.path.join('content', nb_dir, src)
-
-    nb_path = copy_content(nb_dir, src, refresh=refresh, name=name)
-
-    if not os.path.exists(nb_path):
-        raise ValueError("File {0} could not be found".format(nb_path))
-
-    # generate asset paths
-    asset_path, src_dir = _asset_path(nb_path, settings)
+    # start writing out files
     mkdir_p(asset_path)
-    header_lines, body_lines, resources = process_notebook(nb_path, start=start, end=end, src_dir=src_dir)
     he._write_assets(resources, asset_path)
 
     if not notebook.header_saved:
@@ -164,26 +150,36 @@ def _notebook(settings, src, refresh=False, start=None, end=None, name=None):
         lines = '\n'.join(header_lines).encode('utf-8')
         open('_nb_header.html', 'w').write(lines)
 
-
-    return body_lines
-
-def _asset_path(nb_path, settings):
-    asset_dir =  settings.get('NOTEBOOK_ASSETS', 'nb_assets')
-    dir, fn = os.path.split(nb_path)
-    # skip content/NOTEBOOK_DIR
-    dir = '/'.join(dir.split('/')[2:])
-    name, ext = os.path.splitext(fn)
-    src_dir = os.path.join(asset_dir, dir, name)
-    asset_path = os.path.join('output', src_dir)
-    return asset_path, src_dir
-
+    body = preprocessor.configs.htmlStash.store('\n'.join(body_lines),
+                                                safe=True)
+    return body
 notebook.header_saved = False
 
+def generate_notebook_output(settings, src, refresh=False, start=None, end=None, name=None):
+    # copy file to local content store
+    nb_dir =  settings.get('NOTEBOOK_DIR', 'notebooks')
+    nb_path = copy_content(nb_dir, src, refresh=refresh, name=name)
+
+    if not os.path.exists(nb_path):
+        raise ValueError("File {0} could not be found".format(nb_path))
+
+    # generate asset paths
+    asset_path, src_dir = _asset_path(nb_path, settings)
+
+    header_lines, body_lines, resources = process_notebook(nb_path, start=start, end=end, src_dir=src_dir)
+
+    return body_lines, header_lines, resources, asset_path
+
 def process_notebook(nb_path, start=None, end=None, src_dir=None):
+    """
+    At this point we've got all the paths figured out and explicit. 
+
+    Generate HTML and modify it using the original process_body, process_header
+    """
     # Call the notebook converter
     body, resources = he.process_html_notebook(nb_path, start=start, end=end, src_dir=src_dir)
-    soup = BeautifulSoup(body)
 
+    soup = BeautifulSoup(body)
     head = soup.find_all('head')[0]
     body = soup.find_all('body')[0]
 
@@ -195,6 +191,21 @@ def process_notebook(nb_path, start=None, end=None, src_dir=None):
     body_lines = process_body(body_lines)
     
     return header_lines, body_lines, resources
+
+
+def _asset_path(nb_path, settings):
+    """
+    Returns the path to save the asset files and the dir to prepend
+    to <img src="{src_dir}/image.png" />
+    """
+    asset_dir =  settings.get('NOTEBOOK_ASSETS', 'nb_assets')
+    dir, fn = os.path.split(nb_path)
+    # skip content/NOTEBOOK_DIR
+    dir = '/'.join(dir.split('/')[2:])
+    name, ext = os.path.splitext(fn)
+    src_dir = os.path.join(asset_dir, dir, name)
+    asset_path = os.path.join('output', src_dir)
+    return asset_path, src_dir
 
 
 #----------------------------------------------------------------------
